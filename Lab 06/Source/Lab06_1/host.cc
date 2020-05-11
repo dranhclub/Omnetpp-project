f54c17bce295ed4b3b4fc5333df33988a2c513fd
dranhclub <dranh.club@gmail.com>
2020-05-11 11:04:01 +0700

    Push Lab06_1

---
+++ Lab 06/Source/Lab06_1/host.cc
@@ -0,0 +1,109 @@
+#include <string.h>
+#include <omnetpp.h>
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace omnetpp;
+using namespace std;
+
+class Host: public cSimpleModule {
+private:
+    // constant
+    int EXB_SIZE;
+    double TIMEOUT;
+    double MSG_GEN_INTERVAL;
+    double CHANNEL_DELAY = 0.015; // 15ms
+
+    int lastestMsgId = -1;
+
+    // source queue
+    queue<int> SQ;
+
+    // exit buffer
+    queue<int> EXB;
+
+    void generateMessage();
+    void SQtoEXB(); // Chuyển gói tin từ SQ sang EXB
+    void sendMsg(); // Gửi từ EXB đi sang nút khác
+protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+Define_Module(Host);
+
+void Host::initialize() {
+    // Thiết lập các hằng số
+    MSG_GEN_INTERVAL = getParentModule()->par("MSG_GEN_INTERVAL").doubleValue();
+    TIMEOUT = getParentModule()->par("TIMEOUT").doubleValue();
+    EXB_SIZE = par("EXB_SIZE").intValue();
+
+    // Bắt đầu quá trình tạo các gói tin
+    scheduleAt(0, new cMessage("generate"));
+    // Bắt đầu quá trình gửi các gói tin
+    scheduleAt(0, new cMessage("send"));
+}
+
+void Host::handleMessage(cMessage *msg) {
+    // Hoạt động của Source Queue
+    // Tạo tin nhắn theo chu kỳ
+    if (strcmp(msg->getName(), "generate") == 0) {
+        generateMessage();
+        // Nếu EXB còn chỗ thì chuyển sang luôn
+        if (EXB.size() < EXB_SIZE) {
+            SQtoEXB();
+        }
+
+        if (simTime() < TIMEOUT) {
+            scheduleAt(simTime() + MSG_GEN_INTERVAL, msg);
+        } else {
+            cancelEvent(msg);
+        }
+    }
+
+    // Hoạt động của Exit Buffer
+    // Gửi tin nhắn theo chu kỳ
+    if (strcmp(msg->getName(), "send") == 0) {
+        sendMsg();
+        SQtoEXB();
+
+        if (simTime() < TIMEOUT) {
+            scheduleAt(simTime() + CHANNEL_DELAY, msg);
+        } else {
+            cancelEvent(msg);
+        }
+    }
+}
+
+void Host::generateMessage() {
+    SQ.push(++lastestMsgId);
+    EV << "message generated" << endl;
+}
+
+void Host::SQtoEXB(){
+    if (!SQ.empty()){
+        int msgId = SQ.front();
+        SQ.pop();
+        EXB.push(msgId);
+    }
+}
+
+void Host::sendMsg(){
+    if (!EXB.empty()) {
+        // Lấy ra id gói tin chuẩn bị gửi
+        int sentMsgId = EXB.front();
+        EXB.pop();
+
+        // Tạo gói tin (giả lập)
+        cMessage *sentMsg = new cMessage("testMsg");
+
+        // Gắn id cho gói tin
+        cMsgPar *msgParam = new cMsgPar("msgId");
+        msgParam->setLongValue(sentMsgId);
+        sentMsg->addPar(msgParam);
+
+        // Gửi
+        send(sentMsg, "out");
+    }
+}
---
+++ Lab 06/Source/Lab06_1/switch.cc
@@ -0,0 +1,57 @@
+#include <string.h>
+#include <omnetpp.h>
+#include <string>
+
+using namespace omnetpp;
+
+class Switch: public cSimpleModule {
+private:
+    double TIMEOUT;
+    double MSG_GEN_INTERVAL;
+    int *receivedMsgCount;
+    int arrayLength;
+    int intervalCount = 0;
+protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+Define_Module(Switch);
+
+void Switch::initialize() {
+    // Thiết lập các hằng số
+    MSG_GEN_INTERVAL = getParentModule()->par("MSG_GEN_INTERVAL").doubleValue();
+    TIMEOUT = getParentModule()->par("TIMEOUT").doubleValue();
+
+    // Khởi tạo mảng
+    arrayLength = TIMEOUT / MSG_GEN_INTERVAL;
+    receivedMsgCount = new int[arrayLength];
+    memset(receivedMsgCount, 0, arrayLength * sizeof(int));
+
+    scheduleAt(0, new cMessage("nextInterval"));
+}
+
+void Switch::handleMessage(cMessage *msg) {
+
+    // Nhận và đếm số gói tin
+    if (strcmp(msg->getName(), "testMsg") == 0) {
+        EV << "Received testMsg" << endl;
+        receivedMsgCount[intervalCount]++;
+        delete msg;
+    }
+
+    // Chuyển interval
+    if (strcmp(msg->getName(), "nextInterval") == 0) {
+        if (simTime() < TIMEOUT) {
+            intervalCount++;
+            scheduleAt(simTime() + MSG_GEN_INTERVAL, msg);
+        } else {
+            cancelEvent(msg);
+            for (int i = 0; i < arrayLength; i++) {
+                EV << "interval " << i << ", received " << receivedMsgCount[i]
+                          << " messages" << endl;
+            }
+        }
+    }
+}
+
