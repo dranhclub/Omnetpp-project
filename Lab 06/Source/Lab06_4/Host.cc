b0ec5fecd24d54db8080eaed8785f9e22133b2cf
dranhclub <dranh.club@gmail.com>
2020-06-14 09:59:47 +0700

    add Torus2D

--- Lab 06/Source/Lab06_4/Host.cc
+++ Lab 06/Source/Lab06_4/Host.cc
@@ -12,7 +12,7 @@ void Host::initialize() {
     if (strcmp(getName(), "h0_0_0") == 0) {
         cMessage *testMsg = new cMessage("testMsg");
         cMsgPar *msgPar = new cMsgPar("dst");
-        msgPar->setStringValue("h2_1_2");
+        msgPar->setStringValue("h2_1_0");
         testMsg->addPar(msgPar);
         send(testMsg, "port$o", 0);
     }
